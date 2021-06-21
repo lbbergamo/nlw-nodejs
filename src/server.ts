0e4f59623975958f418826072dd866a8c651682d
Leandro Bergamo <leandro.bergamo@hotmail.com>
2021-06-21 01:05:20 -0300

    feat: add project port to process.ENV

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
 
 const app = express();
-
-app.listen(3000,() => console.log('Server is running'))
+require('dotenv').config()
+app.listen(process.env.PORT,() => console.log(`Server is running on port ${process.env.PORT}`))
