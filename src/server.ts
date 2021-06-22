f67363c346213b59d8fec24e9586f8aaa7277bb8
Leandro Bergamo <leandro.bergamo@hotmail.com>
2021-06-21 22:36:30 -0300

    feat: create routes for project

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,11 @@ import express from 'express';
 import "reflect-metadata";
 
 import "./database";
+import { router } from "./routes";
 
 const app = express();
+
+app.use(router)
+
 require('dotenv').config()
 app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`))
