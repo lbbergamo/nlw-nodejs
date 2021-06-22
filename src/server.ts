537f36dc3cdbe084a2b7de661dacadf12d8ab55b
Leandro Bergamo <leandro.bergamo@hotmail.com>
2021-06-21 21:08:43 -0300

    feat: add typeorm of database

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import "reflect-metadata";
+
+import "./database";
 
 const app = express();
 require('dotenv').config()
-app.listen(process.env.PORT,() => console.log(`Server is running on port ${process.env.PORT}`))
+app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`))
