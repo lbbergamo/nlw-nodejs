34b4cffe7ca71ada88a74009441e3da46f95bcb3
Leandro Bergamo <leandro.bergamo@hotmail.com>
2021-06-22 22:43:33 -0300

    feat: add express-async-errors and handling error to middleware

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import "reflect-metadata";
-
+import "express-async-errors";
 import "./database";
 import { router } from "./routes";
 
 const app = express();
 app.use(express.json())
 app.use(router)
-
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) return res.status(400).json({ error: err.message })
+    return res.status(500).json({status:"error",message: "Internal Server Error"})
+})
 require('dotenv').config()
 app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`))
