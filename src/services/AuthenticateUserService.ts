f006abcafad259d9ef7470561be5c9dca95df280
Leandro Bergamo <leandro.bergamo@hotmail.com>
2021-06-23 22:04:33 -0300

    feat: create authenticateUserController and authenticateUserService

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,26 @@
+import { getCustomRepository } from "typeorm"
+import { UsersRepositories } from "../repositories/UsersRepositories"
+import { compare } from "bcryptjs"
+import { sign } from "jsonwebtoken"
+import { Subject } from "typeorm/persistence/Subject";
+
+interface IAuthenticateRequest {
+    email: string;
+    password: string;
+}
+
+class AuthenticateUserService {
+    async execute({ email, password }: IAuthenticateRequest) {
+        const usersRepository = getCustomRepository(UsersRepositories);
+
+        const user = await usersRepository.findOne({ email })
+        if (!user) throw new Error("Email/Password incorrect")
+
+        const passwordMatch = await compare(password, user.password)
+        if (!passwordMatch) throw new Error("Email/Password incorrect")
+
+        const token = await sign({ email: user.email }, process.env.TOKEN, { subject: user.id, expiresIn: "3600" })
+    }
+}
+
+export { AuthenticateUserService }
