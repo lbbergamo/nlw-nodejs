5901af1c7c1c72a5838993b293d310d8f4d6d0a6
Leandro Bergamo <leandro.bergamo@hotmail.com>
2021-06-25 00:11:33 -0300

    chore: add cors to project

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,10 @@ import "reflect-metadata";
 import "express-async-errors";
 import "./database";
 import { router } from "./routes";
+import cors from "cors";
 
 const app = express();
+app.use(cors)
 app.use(express.json())
 app.use(router)
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
